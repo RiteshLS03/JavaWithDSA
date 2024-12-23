f419a06d280b3534cd391426c94df4952186fd97
Ritesh Narwade <riteshnarwade03@gmail.com>
2024-12-23 14:15:13 +0530

    Data Types in Java

---
+++ Java/Hello.java
@@ -0,0 +1,34 @@
+
+// public class Hello {
+//     // public static void main(String args[]) {
+//         public static void main(String args[]){
+
+//         System.out.print("DKFLKSJ");
+//     }
+// }
+// // Boiler Plate code
+
+// public class Hello {
+//     public static void main(String[] args) {
+//         System.out.print("1 21 21 32 4 5 6");
+//     }
+// }
+
+// public class Hello {
+//     public static void main(String args[]){
+//         System.out.println("****");
+//         System.out.println("***");
+//         System.out.println("**");
+//         System.out.println("*");
+//     }
+// }
+
+// public class Hello {
+//     public static void main(String[] args) {
+
+//         int a = 10;
+//         int b = 20;
+//         int c = 30;
+//         System.out.print(a);
+//     }
+// }
