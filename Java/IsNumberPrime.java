64215eeca2e6dce1bf9e81ecedbb8f631aebac4a
Ritesh Narwade <riteshnarwade03@gmail.com>
2025-03-06 20:53:14 +0530

    Prime number in a range

--- Java/IsNumberPrime.java
+++ Java/IsNumberPrime.java
@@ -10,29 +10,75 @@
 // }
 // }
 
+// import java.util.*;
+
+// public class IsNumberPrime {
+// public static void main(String args[]) {
+// Scanner scan = new Scanner(System.in);
+// int t = scan.nextInt();
+
+// for (int i = 0; i < t; i++) {
+// int n = scan.nextInt();
+
+// int count = 0;
+// for (int div = 2; div * div <= n; div++) {
+// if (n % div == 0) {
+// count++;
+// break;
+// }
+// }
+// if (count == 0) {
+// System.out.println("PRIME");
+// } else {
+// System.out.println("NOT PRIME");
+// }
+// }
+
+// }
+// }
+
+// import java.util.*;
+
+// public class IsNumberPrime {
+// public static void main(String args[]) {
+// Scanner scan = new Scanner(System.in);
+// int t = scan.nextInt();
+
+// for (int i = 0; i < t; i++) {
+// int n = scan.nextInt();
+
+// int count = 0;
+// for (int div = 2; div * div <= n; div++) {
+// if (n % div == 0) {
+// count++;
+// break;
+// }
+// }
+// if (count == 0) {
+// System.out.println("PRIME");
+// } else {
+// System.out.println("NOT PRIME");
+// }
+// }
+// }
+// }
+
 import java.util.*;
 
 public class IsNumberPrime {
+
     public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        int t = scan.nextInt();
 
-        for (int i = 0; i < t; i++) {
-            int n = scan.nextInt();
+        Scanner sc = new Scanner(System.in);
 
-            int count = 0;
-            for (int div = 2; div * div <= n; div++) {
-                if (n % div == 0) {
-                    count++;
-                    break;
-                }
-            }
-            if (count == 0) {
-                System.out.println("PRIME");
-            } else {
-                System.out.println("NOT PRIME");
-            }
+        System.out.println("Enter the first number: ");
+        int Num1 = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int Num2 = sc.nextInt();
+
+        for (int i = Num1; i <= Num2; i++) {
+            System.out.println(Num1);
+            Num1++;
         }
-
     }
 }
